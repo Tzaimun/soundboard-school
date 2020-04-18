a072059dd1080365e564e21dbf56cec29e70054d
Tijmen <tijmen.hilgenkamp@gmail.com>
2020-04-18 12:29:51 +0200

    Login page front-end

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -2,7 +2,10 @@ import Api from '@/services/Api'
 
 export default {
   register (credentials) {
-    console.log('Authentication service log.')
     return Api().post('register', credentials)
+  },
+  login (credentials) {
+    console.log('Authentication service log.')
+    return Api().post('login', credentials)
   }
 }

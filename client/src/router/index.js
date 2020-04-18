6a6253e89f6c007daf9d8c9945bec293490a6054
Tijmen <tijmen.hilgenkamp@gmail.com>
2020-04-18 12:30:59 +0200

    Revert "Login page front-end"
    This reverts commit a072059dd1080365e564e21dbf56cec29e70054d.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
 import Register from '@/components/Register'
-import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -17,11 +16,6 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
     }
   ]
 })
--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -2,10 +2,7 @@ import Api from '@/services/Api'
 
 export default {
   register (credentials) {
-    return Api().post('register', credentials)
-  },
-  login (credentials) {
     console.log('Authentication service log.')
-    return Api().post('login', credentials)
+    return Api().post('register', credentials)
   }
 }
